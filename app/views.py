48e7803e8adab7ffe6ae649173e15ac16cccfeba
Imanol Aranzadi <ima@Imanols-MacBook-Pro.local>
2015-07-01 19:21:21 -0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,29 @@
+from flask import render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
+from app import app
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    return render_template('index.html', name="Imanol", current_time=datetime.utcnow(), form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
